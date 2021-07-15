66442de80270f9b26e6f55d130a7c5e1fbc6fcd0
cws3299 <cws3299@gmail.com>
2021-07-15 11:37:16 +0900

    20210714에 푼 문제 0715에 업로드 algo

---
+++ bfs/16397 탈출/16397.py
@@ -0,0 +1,60 @@
+import sys
+sys.stdin = open('16397.txt','r')
+from collections import deque
+input = sys.stdin.readline
+
+n,t,g = map(int, input().split())
+
+q = deque()
+visit = [0]*(100000)
+answer = False
+
+cnt = 0
+q.append([n,0])
+visit[n] = 1
+while q:
+    now,day = q.popleft()
+
+    if day > t:
+        break
+    
+    if now == g:
+        answer = day
+        break 
+
+
+    for k in range(2):
+        if k == 0:
+            if 0<= now+1 <=99999:
+                if visit[now+1] == 0:
+                    visit[now+1] = 1
+                    q.append([now+1,day+1])
+        else:
+            now2 = now * 2
+            noww = now2
+            now2 = str(now2)
+            now2_lst = list(now2)
+            _max = int(now2_lst[0])
+            _max -= 1
+            now2_lst[0] = _max
+            now3 = ''
+            for k in range(len(now2_lst)):
+                now3 += str(now2_lst[k])
+            now3 = int(now3)
+            if now3 < 0:
+                now3 = 0
+            if visit[now3] == 0 and 0<= noww <= 99999 and 0<=now3 <=99999:
+                visit[now3] = 1
+                q.append([now3,day+1])
+
+if n == g:
+    print(0)
+else:
+    if answer == False:
+        print("ANG")
+    else:
+        print(answer)
+        
+
+
+
